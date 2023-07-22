87dec5eab09b087bc1d4ec7aa2823f798a269d2f
Rouven <rouven@datarevenue.com>
2023-07-22 12:01:19 +0200

    Add chatbox

---
+++ frontend/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    if (!message) return;
+    setIsTyping(true);
+    window.scrollTo(0, 1e10);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+
+    setMessage("");
+
+    fetch("http://localhost:8000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chats,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        msgs.push(data.output);
+        setChats(msgs);
+        setIsTyping(false);
+        window.scrollTo(0, 1e10);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <main>
+      <h1>Text Swap</h1>
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+      <div className={isTyping ? "" : "hide"}>
+        <p>
+          <i>{isTyping ? "Typing" : ""}</i>
+        </p>
+      </div>
+
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Lemme know what ya wanna swap, I'll do it for ya, bro..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
+  );
+}
+
+export default App;
